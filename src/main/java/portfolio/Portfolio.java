c0e65f5596adb35a0f624cdfd996a35d9ea29d7e
Pascal Herzog (s) <pascal.herzog@students.fhnw.ch>
2020-03-02 14:32:08 +0100

    Finished Assignment

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,49 @@
 package portfolio;
 
-public class Portfolio {
+import portfolio.investments.Investment;
+
+import java.util.ArrayList;
+
+public class Portfolio<T extends Investment> {
+
+    private ArrayList<T> investments = new ArrayList<>();
+
+    public boolean contains(T inv) {
+        return investments.contains(inv);
+    }
+
+    public void buy(T inv) {
+        String invTitel = inv.getTitel();
+        T ownedInvestment = getShare(invTitel);
+        if ( ownedInvestment == null){
+           investments.add(inv);
+        }
+        else
+        {
+            ownedInvestment.setCount(ownedInvestment.getCount()+inv.getCount());
+        }
+    }
+
+    public void sell(String inv, int amount) {
+        T investment = getShare(inv);
+        if (!(investment==null)){
+            investment.setCount(investment.getCount()-amount);
+            if (investment.getCount()<=0){
+                investments.remove(investment);
+            }
+        }
+
+    }
+
+    public T getShare(String inv) {
+        for (T investment : investments) {
+            if (investment.getTitel().equals(inv)) {
+                return investment;
+            }
+        }
+        return null;
+    }
+
+
     //TODO Complete this implementation and make it generic
 }
